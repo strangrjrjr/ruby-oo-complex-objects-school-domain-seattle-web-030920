db994c85005a584fa9a5a5d48d1f1c67f30c77b9
Roger Robertson Jr <rjrjr@pm.me>
2020-03-10 17:20:16 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(new_student, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << new_student
+        else
+            @roster[grade] << new_student
+        end
+    end
+
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort
+        result = {}
+        @roster.each do |grade, student|
+            result[grade] = student.sort
+        end
+        result
+    end
+
+end
